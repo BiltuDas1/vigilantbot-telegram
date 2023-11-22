035fafeaa8f200e6b2639dad80ead8ced4cef978
Biltu Das <billionto@gmail.com>
2023-11-22 11:47:58 +0530

    Updated Github Actions

--- .github/scripts/release/getversion.sh
+++ .github/scripts/release/getversion.sh
@@ -5,7 +5,7 @@ VersionType=$(echo $Ver | cut -f2 -d-)
 
 echo "Version=$Version" >> $GITHUB_ENV
 
-if [[ $VersionType == "beta" ]]; then
+if [[ $VersionType == "beta" || $VersionType == "alpha" ]]; then
     echo "Prerelease=true" >> $GITHUB_ENV
 else
     echo "Prerelease=false" >> $GITHUB_ENV

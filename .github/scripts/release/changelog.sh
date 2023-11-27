5ccf6bb978ff930be01574505f363d3aee50afef
Biltu Das <billionto@gmail.com>
2023-11-27 18:27:22 +0530

    Fixed CHANGELOG.md script

--- .github/scripts/release/changelog.sh
+++ .github/scripts/release/changelog.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 # Full version with version type
 FullVer=$(cat version.txt)
+PartialVer=$(echo $FullVer | cut -f1 -d-)
 
 # Get old version from CHANGELOG.md
 OLD_FullVer=$(grep -i -o1 "^## \[\(.\+\)\]" CHANGELOG.md | head -2 | tail -1 | cut -d\[ -f2 | cut -d\] -f1)
+OLD_PartialVer=$(echo $OLD_FullVer | cut -f1 -d-)
 
 result=$(grep -c "\[$FullVer\]" CHANGELOG.md)
 # Updates the CHANGELOG.md if version.txt version information doesn't avaliable into the CHANGELOG.md
 if [[ $result -eq 0 ]]; then
     # Replaces the unreleased version with compared url
     if [[ "$OLD_FullVer" == "unreleased" ]]; then
-        sed -i -e 's/\[unreleased\]: https:\/\/github.com\/BiltuDas1\/vigilantbot-telegram/\['$FullVer'\]: https:\/\/github.com\/BiltuDas1\/vigilantbot-telegram\/tree\/'$FullVer'/' CHANGELOG.md
+        sed -i -e 's/\[unreleased\]: https:\/\/github.com\/BiltuDas1\/vigilantbot-telegram/\['$FullVer'\]: https:\/\/github.com\/BiltuDas1\/vigilantbot-telegram\/tree\/'$PartialVer'/' CHANGELOG.md
     else
-        sed -i -e 's/\[unreleased\]: https:\/\/github.com\/BiltuDas1\/vigilantbot-telegram\/compare\/'$OLD_FullVer'...main/\['$FullVer'\]: https:\/\/github.com\/BiltuDas1\/vigilantbot-telegram\/compare\/'$OLD_FullVer'...'$FullVer'/' CHANGELOG.md
+        sed -i -e 's/\[unreleased\]: https:\/\/github.com\/BiltuDas1\/vigilantbot-telegram\/compare\/'$OLD_PartialVer'...main/\['$FullVer'\]: https:\/\/github.com\/BiltuDas1\/vigilantbot-telegram\/compare\/'$OLD_PartialVer'...'$PartialVer'/' CHANGELOG.md
     fi
 
     # Adds the version no and release date to unreleased version
@@ -27,7 +29,7 @@ if [[ $result -eq 0 ]]; then
     ) > CHANGELOG.md.swap
 
     # Linking unrelease version with compare url
-    echo \[unreleased\]: https://github.com/BiltuDas1/vigilantbot-telegram/compare/$FullVer...main >> CHANGELOG.md.swap
+    echo \[unreleased\]: https://github.com/BiltuDas1/vigilantbot-telegram/compare/$PartialVer...main >> CHANGELOG.md.swap
 
     # Remove the old changelog and replace it with new one
     rm CHANGELOG.md
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,6 @@
 ## [unreleased]
-There's Nothing but Crickets ¯\\_(ツ)_/¯
-
-## [0.0.1-alpha] - 2023-11-27
 - Added bot required permission checker
 - Added [SpamWatch API](https://github.com/SpamWatch) to filter user
 
 ---
-[0.0.1-alpha]: https://github.com/BiltuDas1/vigilantbot-telegram/tree/0.0.1-alpha
-[unreleased]: https://github.com/BiltuDas1/vigilantbot-telegram/compare/0.0.1-alpha...main
+[unreleased]: https://github.com/BiltuDas1/vigilantbot-telegram
